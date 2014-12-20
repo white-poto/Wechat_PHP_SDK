6f634c668d740625499cc1e8c10d1854183a1964
huyanping <hypxm@qq.com>
2014-12-20 16:46:18 +0800

    添加example

--- examples/TextMessage.php
+++ examples/TextMessage.php
@@ -8,9 +8,21 @@
  * 主动向微信发送消息
  */
 
+
 define('WECHAT_APP_ID', 'your app id');
 define('WECHAT_SECRET', 'your secret');
 
+$to_user = 'to_user_open_id';
+$text = 'hello';
+$text_message = new \Jenner\Zebra\Wechat\Client\Message\TextMessage($to_user, $text);
+$text_message->send();
+
+
+/**
+ * 或者 你可以这样
+ */
+
+\Jenner\Zebra\Wechat\Client\WechatClient::registerAuthInfo('your app_id', 'your secret');
 $to_user = 'to_user_open_id';
 $text = 'hello';
 $text_message = new \Jenner\Zebra\Wechat\Client\Message\TextMessage($to_user, $text);
