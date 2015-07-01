1d3393a18d148bce577b67be58c72f5fa218beaa
huyanping <hypxm@qq.com>
2015-07-01 09:22:16 +0800

    rename response namespace and class name

---
+++ src/Jenner/Wechat/Response/Image.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 14-11-13
+ * Time: 下午1:46
+ */
+
+namespace Jenner\Wechat\Response;
+
+
+class Image extends AbstractXml
+{
+
+    public function __construct($to_user, $from_user, $media_id)
+    {
+        parent::__construct($to_user, $from_user);
+        $this->replace['MediaId'] = $media_id;
+    }
+
+    protected function initTemplate()
+    {
+        $this->template = <<<XML
+<xml>
+<ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+<FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+<CreateTime>{CreateTime}</CreateTime>
+<MsgType><![CDATA[image]]></MsgType>
+<Image>
+<MediaId><![CDATA[{MediaId}]]></MediaId>
+</Image>
+</xml>
+XML;
+    }
+}
+
+
